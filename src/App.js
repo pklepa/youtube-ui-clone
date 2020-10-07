e91b2729ddd302cac77ae439a0361c22098c71e4
klepa <pedro.klepa@gmail.com>
2020-10-07 10:50:52 -0300

    Add ESLint, Prettier, Styled Components and Styled Icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import './assets/styles/reset.css'
-import './assets/fonts/fonts.css'
+import "./assets/styles/reset.css";
+import "./assets/fonts/fonts.css";
 
 function App() {
   return (
     <>
-      <h1 style={{fontFamily: 'Roboto'}} >The Odin Project</h1>
+      <h1 style={{ fontFamily: "Roboto" }}>The Odin Project</h1>
     </>
   );
 }
