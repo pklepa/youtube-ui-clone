3ba39504de7dda7c80108d5b4da50e7a58159601
klepa <pedro.klepa@gmail.com>
2020-10-07 10:38:42 -0300

    Add reset.css and fonts.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import './assets/styles/reset.css'
+import './assets/fonts/fonts.css'
+
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 style={{fontFamily: 'Roboto'}} >The Odin Project</h1>
     </>
   );
 }
